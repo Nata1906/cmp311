c51ea58eed635b0a87d0b56964212dbc47dd0b5a
Klimuk N.S. <klimuknatka@gmail.com>
2024-10-13 13:15:02 +0300

    Update ip a.sh

--- ip a.sh
+++ ip a.sh
@@ -5,11 +5,10 @@ network:
     enp0s3:
       addresses:
       - 192.168.1.100/24
-      gateway4: 192.168.1.255
+      gateway4: 192.168.1.1
       nameservers:
         addresses:
         - 8.8.8.8
   version: 2
  
  sudo netplan apply (щоб застосувати)
-********************
